12480e9eb1e978b3fae7d867f33a77e75655327d
Tim Farnam <twfarnam@gmail.com>
2018-03-30 12:50:27 -0600

    real number of steps

--- src/generator.py
+++ src/generator.py
@@ -45,7 +45,7 @@ def make_generator(target_size=None, batch_size=None, category_ids=None):
                 ])
                 yield (x, y)
 
-    # return (train_generator, steps, )
-    return (train_generator, 3, )
+    return (train_generator, steps, )
+    # return (train_generator, 3, )
 
 
